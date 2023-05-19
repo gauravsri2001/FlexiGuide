1186336dbf1c0d8b36bf0ac26c5ae580179cbbc2
Gaurav Srivastava <2001srigaurav@gmail.com>
2023-05-19 23:44:51 +0530

    add

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+// import React from 'react'
+// import Body_diff from './Body_diff';
+// import Logo from '../pics/Logo.png';
+// import banner_img from '../pics/banner_img.png';
 
-const BodyPart = () => {
+// const BodyPart = () => {
+//   return (
+//     <div>
+//       <Body_diff imgUrl={Logo} />
+//       {/* <Body_diff imgUrl={banner_img} /> */}
+//     </div>
+//   )
+// }
+
+// export default BodyPart
+
+import { Stack } from '@mui/material';
+import React from 'react';
+import Logo from '../pics/Logo.png';
+
+
+const BodyPart = ({item, setBodyPart, bodyPart, imgUrl}) => {
   return (
-    <div>BodyPart</div>
+    <Stack
+    type='button'
+    alignItems={'center'}
+    justifyContent={'center'}
+    className='bodyPart-card'
+    sx={{
+        borderTop: bodyPart === item ? '4px solid #ff2625' : '',
+        backgroundColor: '#fff',
+            borderBottomLeftRadius: '20px',
+            width: '270px',
+            height: '280px',
+            cursor: 'pointer',
+            gap: '47px'
+    }}
+       
+    >
+
+    <img src={Logo} alt='dumbbell' style={{width: '40px', height: '40px'}} />
+    
+    </Stack>
   )
 }
 
-export default BodyPart
+export default BodyPart;
