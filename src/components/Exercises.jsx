a1bcbd6de73af13820df17d746741bbbfa0f551e
Gaurav Srivastava <2001srigaurav@gmail.com>
2023-05-31 01:13:05 +0530

    add

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,39 +1,3 @@
-// import React from 'react';
-// import { Box, Pagination, Stack, Typography } from '@mui/material';
-
-// import ExerciseCard from './ExerciseCard';
-
-// const Exercises = ({exercises, setExercises, bodyPart}) => {
-//   //console.log(exercises);
-//   return (
-//     <Box id='exercises'
-//     sx={{mt: {lg: '110px'}}}
-//     mt={'50px'}
-//     p={'20px'}
-//     >
-
-//     <Typography variant='h3' mb='46px'>
-//       Showing results
-//     </Typography>
-
-//     <Stack direction={'row'}
-//     sx={{gap:{lg:'110px', xs: '50px'}}}
-//     flexWrap={'wrap'}
-//     justifyContent={'center'}>
-//     {exercises.map((exercise, index) => (
-//       <ExerciseCard key={index} exercise={exercise} />
-      
-//     ))}
-    
-
-
-//     </Stack>
-
-//     </Box>
-//   )
-// }
-
-// export default Exercises;
 
 
 
@@ -83,7 +47,9 @@ const Exercises = ({exercises,setExercises,bodyPart}) => {
       </Stack>
       <Stack mt='100px' alignItems='center'>
         {exercises.length>9 && (
-          <Pagination color='standard' shape='rounded' defaultPage={1} count={Math.ceil(exercises.length/exercisesPerPage)} page={currentPage} onChange={paginate} size='large'/>
+          <Pagination color='primary' shape='rounded' defaultPage={1} count={Math.ceil(exercises.length/exercisesPerPage)} page={currentPage} onChange={paginate} size='large'
+            sx={{'& .MuiPaginationItem-root' : {color: 'white'}}}
+          />
         )}
       </Stack>
     </Box>
